fbaec6d03266ddef91999d80d3b2246208dc5513
Andre Lussier <Dremon7@hotmail.com>
2019-10-21 22:25:32 -0400

    Initial commit

--- venv/Include/Eh.py
+++ venv/Include/Eh.py
@@ -41,4 +41,60 @@ while a < 1000:
     a, b = b, a + b
 
 
+x = int(input("Please enter an integer: "))
+
+if x < 0:
+    x = 0
+    print('Negative chagned to zero')
+elif x == 0:
+    print('Zero')
+elif x == 1:
+    print('Single')
+else:
+    print('More')
+
+words = ['cat','window','defensestrate']
+
+for w in words:
+    print(w, len(w))
+
+# Strategy: Iterate over a copy
+for user, status in user.copy.items():
+    if stats == 'inactive':
+        del users[user]
+
+# Strategy: Create a new collection
+active_users = {}
+for user, status in users.items():
+    if status == 'active':
+        active_users[user] = status
+
+for i in range(5):
+    print(i)
+
+range(5,10)
+
+range(0,10,3)
+
+range(-10,-100,-30)
+
+a = ['Mary','had','a','little','lamb']
+for i in range(len(a)):
+    print(i, a[i])
+
+sum(range(4))
+
+
+for n in range(2,10):
+    for x in range(2, n):
+        if n % x == 0:
+            print(n, 'equals',x,'*',n//x)
+            break
+    else:
+        # loop fell through without find a factor
+        print(n, 'is a prime number')
+
+
+
+
 
