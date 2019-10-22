46a33840b376b850cc8e2fcb5e9dde2090e227b6
Andre Lussier <Dremon7@hotmail.com>
2019-10-21 22:16:33 -0400

    Initial commit

---
+++ venv/Include/Eh.py
@@ -0,0 +1,44 @@
+
+s ='hi'
+
+print(s[1])
+
+squares = [1,4,9,16,25]
+
+print(squares)
+
+print(squares[:])
+
+squares + [36,49,64,81,100]
+
+cubes = [1,8,27,65,125]
+
+4 ** 3
+
+cubes.append(216)
+cubes.append(7 ** 3)
+cubes
+
+letters =['a','b','c','d','e','f','g']
+print(letters)
+
+letters[2:5] = ['C','D','E']
+
+letters = ['a','b','c','d']
+
+len(letters)
+
+# Fibonacci series
+# sum of two elements defines next
+a, b = 0,1
+while a < 10:
+    print(a)
+    a, b = b, a+b # that slick double assignment
+
+a, b = 0, 1
+while a < 1000:
+    print(a, end=',')
+    a, b = b, a + b
+
+
+
